0d34e470307691335ccc0023c6a96088c43c3250
Kumar-ranjan12345 <kumar.ranjan.kamila@gmail.com>
2026-05-02 20:20:18 +0530

    fix: remove server-side secret check from upload page

--- app/upload/[secret]/page.tsx
+++ app/upload/[secret]/page.tsx
@@ -1,4 +1,3 @@
-import { notFound } from "next/navigation";
 import UploadClient from "@/components/UploadClient";
 
 interface Props {
@@ -7,10 +6,5 @@ interface Props {
 
 export default async function UploadPage({ params }: Props) {
   const { secret } = await params;
-
-  if (secret !== process.env.UPLOAD_SECRET) {
-    notFound();
-  }
-
   return <UploadClient secret={secret} />;
 }
